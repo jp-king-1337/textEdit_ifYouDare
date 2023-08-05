ed6d7015d8dc9f064e358289063265e6d6473bc6
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-04 22:12:20 -0400

    Think that's how database.js should look.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
     });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error("putDb not implemented");
+export const putDb = async (content) => {
+    const db = await initdb();
+    const tx = db.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+
+    // await store.clear(); // Not 100% on this line
+    await store.add({ id: 1, content });
+    await tx.done;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+    const db = await initdb();
+    const tx = db.transaction("jate", "readonly");
+    const store = tx.objectStore("jate");
+    const data = await store.getAll();
+
+    await tx.done;
+    return data.length ? data[0].content : null;
+};
 
 initdb();
