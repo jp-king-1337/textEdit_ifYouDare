ba0a8c49583563e1ba1052df80b7417db1b21774
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-04 21:58:29 -0400

    I do NOT like that it's called butInstall. Confusing name for a button.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -13,7 +13,25 @@ window.addEventListener("beforeinstallprompt", (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async () => { });
+butInstall.addEventListener("click", async () => {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+
+        const result = await deferredPrompt.userChoice;
+
+        if (result.outcome === "accepted") {
+            console.log("User accepted PWA installation.");
+        } else {
+            console.log("User rejected PWA installation.");
+        }
+
+        deferredPrompt = null;
+
+        butInstall.style.display = "none";
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener("appinstalled", (event) => { });
+window.addEventListener("appinstalled", (event) => {
+    console.log("Successfully installed PWA.");
+});
