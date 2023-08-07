e8c5ad2d18eb15b8a0580293dd72b9ac33debce0
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-07 15:44:59 -0400

    JD said the only files we need to change at all are client/src/js/database.js and client/src/js/install.js, so I overwrote all but those files with the starter code and will now use this branch to finish the homework.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,68 +1,30 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-    return {
-        mode: "development",
-        entry: {
-            main: "./src/js/index.js",
-            install: "./src/js/install.js"
-        },
-        output: {
-            filename: "[name].bundle.js",
-            path: path.resolve(__dirname, "dist"),
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: path.resolve(__dirname, "/src/index.html"),
-                chunks: ["main"],
-            }),
-            new WebpackPwaManifest({
-                name: "textEdit_ifYouDare",
-                short_name: "textEdit",
-                description: "This is a text editor.",
-                background_color: "#fff",
-                theme_color: "#fff",
-                icons: [
-                    {
-                        src: path.resolve("src/images/logo.png"),
-                        sizes: [72, 96, 120, 128, 144, 152, 180, 192, 256, 384, 512], // https://stackoverflow.com/questions/48839338/which-icon-sizes-are-required-for-progressive-web-apps-pwa-as-of-q1-2018
-                        destination: path.join("assets", "icons"),
-                    }
-                ]
-            }),
-            new InjectManifest({
-                swSrc: "./src-sw.js",
-                swDest: "service-worker.js"
-            })
-        ],
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      
+    ],
 
-        module: {
-            rules: [
-                // https://webdevetc.com/programming-tricks/javascript/webpack/guide-to-webpack-css-style-loader/
-                {
-                    test: /\.css$/, // I know regex at least
-                    use: ["style-loader", "css-loader"],
-                },
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env"],
-                        }
-                    }
-                }
-            ],
-        },
-    };
+    module: {
+      rules: [
+        
+      ],
+    },
+  };
 };
-
-console.log("HtmlWebpackPlugin Config:", new HtmlWebpackPlugin().options);
